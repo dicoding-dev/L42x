0f923a3fc8d01e6330a1611172a13c10dc663aed
Ash-Shiddiqul Akbar Hidayat <akbar@dicoding.com>
2022-07-15 09:52:49 +0700

    added new util class

---
+++ src/Illuminate/Support/Util.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Illuminate\Support;
+
+use Closure;
+
+class Util
+{
+    /**
+     * Return the default value of the given value.
+     *
+     * @param  mixed  $value
+     * @param  mixed  ...$args
+     * @return mixed
+     */
+    public static function unwrapIfClosure($value, ...$args)
+    {
+        return $value instanceof Closure ? $value(...$args) : $value;
+    }
+}
