214d2959fa0aa4e072cc835f81780829794a91e0
Ash-Shiddiqul Akbar Hidayat <akbar@dicoding.com>
2022-07-26 08:28:47 +0700

    static

--- tests/Support/SupportUtilTest.php
+++ tests/Support/SupportUtilTest.php
@@ -10,7 +10,7 @@ class SupportUtilTest extends TestCase
     public function testUnwrapIfClosure(): void
     {
         $this->assertSame('foo', Util::unwrapIfClosure('foo'));
-        $this->assertSame('foo', Util::unwrapIfClosure(function () {
+        $this->assertSame('foo', Util::unwrapIfClosure(static function () {
             return 'foo';
         }));
     }
