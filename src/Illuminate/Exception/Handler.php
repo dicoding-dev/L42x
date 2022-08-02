231c90cc0c75facb8e38844f29f832924cf008db
Ash-Shiddiqul Akbar Hidayat <akbar@dicoding.com>
2022-08-02 17:26:01 +0700

    move the code here because it has no context to be ran inside the loop

--- src/Illuminate/Exception/Handler.php
+++ src/Illuminate/Exception/Handler.php
@@ -220,33 +220,30 @@ class Handler {
 	 *
 	 * @param  \Exception  $exception
 	 * @param  bool  $fromConsole
-	 * @return void
 	 */
 	protected function callCustomHandlers($exception, $fromConsole = false)
 	{
+        if ($exception instanceof HttpExceptionInterface) {
+            $code = $exception->getStatusCode();
+        }
+
+        // If the exception doesn't implement the HttpExceptionInterface, we will just
+        // use the generic 500 error code for a server side error. If it implements
+        // the HttpException interfaces we'll grab the error code from the class.
+        else {
+            $code = 500;
+        }
+
 		foreach ($this->handlers as $handler)
 		{
 			// If this exception handler does not handle the given exception, we will just
 			// go the next one. A handler may type-hint an exception that it handles so
 			//  we can have more granularity on the error handling for the developer.
-			if ( ! $this->handlesException($handler, $exception))
-			{
-				continue;
-			}
-			elseif ($exception instanceof HttpExceptionInterface)
-			{
-				$code = $exception->getStatusCode();
-			}
+            if (! $this->handlesException($handler, $exception)) {
+                continue;
+            }
 
-			// If the exception doesn't implement the HttpExceptionInterface, we will just
-			// use the generic 500 error code for a server side error. If it implements
-			// the HttpException interfaces we'll grab the error code from the class.
-			else
-			{
-				$code = 500;
-			}
-
-			// We will wrap this handler in a try / catch and avoid white screens of death
+            // We will wrap this handler in a try / catch and avoid white screens of death
 			// if any exceptions are thrown from a handler itself. This way we will get
 			// at least some errors, and avoid errors with no data or not log writes.
 			try
@@ -316,7 +313,12 @@ class Handler {
 
 		$expected = $parameters[0];
 
-		return ! $expected->getClass() || $expected->getClass()->isInstance($exception);
+        $type = $expected->getType();
+        if (!$type || $type->isBuiltin()) {
+            return false;
+        }
+
+        return (new \ReflectionClass($expected->getType()->getName()))->isInstance($exception);
 	}
 
 	/**
