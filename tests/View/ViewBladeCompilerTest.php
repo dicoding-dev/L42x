57d58640d2633dd7dfcd129968aa4192b8872b8b
Rizqy H <rizqy@dicoding.com>
2023-08-14 11:19:32 +0700

    add test `testClassesAreConditionallyCompiledFromArray`

--- tests/View/ViewBladeCompilerTest.php
+++ tests/View/ViewBladeCompilerTest.php
@@ -462,6 +462,15 @@ empty
     }
 
 
+    public function testClassesAreConditionallyCompiledFromArray()
+    {
+        $string = "<span @class(['font-bold', 'mt-4', 'ml-2' => true, 'mr-2' => false])></span>";
+        $expected = "<span class=\"<?php echo \Illuminate\Support\Arr::toCssClasses(['font-bold', 'mt-4', 'ml-2' => true, 'mr-2' => false]); ?>\"></span>";
+
+        $this->assertEquals($expected, $this->compiler->compileString($string));
+    }
+
+
 	protected function getFiles()
 	{
 		return m::mock('Illuminate\Filesystem\Filesystem');
