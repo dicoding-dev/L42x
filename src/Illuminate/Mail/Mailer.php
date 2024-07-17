2f220e9955767d7f165f0a6025b51367faf22197
AlexzPurewoko <purwoko908@gmail.com>
2024-07-17 10:35:32 +0700

    fix some methods

--- src/Illuminate/Mail/Mailer.php
+++ src/Illuminate/Mail/Mailer.php
@@ -1,6 +1,7 @@
 <?php namespace Illuminate\Mail;
 
 use Closure;
+use Illuminate\Queue\Jobs\Job;
 use Illuminate\Support\Str;
 use Illuminate\Log\Writer;
 use Illuminate\View\Factory;
@@ -13,6 +14,10 @@ use Symfony\Component\Mailer\Transport\TransportInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Email;
 
+/**
+ * Tested At:
+ * {@see MailMailerTest}
+ */
 class Mailer {
 
 	/**
@@ -164,10 +169,10 @@ class Mailer {
 	 * @param  string|array  $view
 	 * @param  array  $data
 	 * @param  \Closure|string  $callback
-	 * @param  string  $queue
+	 * @param  ?string  $queue
 	 * @return mixed
 	 */
-	public function later(int $delay, $view, array $data, $callback, $queue = null): mixed
+	public function later(int $delay, string|array $view, array $data, Closure|string $callback, ?string $queue = null): mixed
     {
 		$callback = $this->buildQueueCallable($callback);
 
@@ -184,7 +189,7 @@ class Mailer {
 	 * @param  \Closure|string  $callback
 	 * @return mixed
 	 */
-	public function laterOn($queue, $delay, $view, array $data, $callback): mixed
+	public function laterOn(string $queue, int $delay, string|array $view, array $data, Closure|string $callback): mixed
     {
 		return $this->later($delay, $view, $data, $callback, $queue);
 	}
@@ -195,9 +200,11 @@ class Mailer {
 	 * @param  mixed  $callback
 	 * @return mixed
 	 */
-	protected function buildQueueCallable($callback): mixed
+	protected function buildQueueCallable(mixed $callback): mixed
     {
-		if ( ! $callback instanceof Closure) return $callback;
+		if (!$callback instanceof Closure) {
+            return $callback;
+        }
 
 		return serialize(new SerializableClosure($callback));
 	}
@@ -209,10 +216,9 @@ class Mailer {
 	 * @param  array  $data
 	 * @return void
 	 */
-	public function handleQueuedMessage($job, $data): void
+	public function handleQueuedMessage(Job $job, array $data): void
     {
 		$this->send($data['view'], $data['data'], $this->getQueuedCallable($data));
-
 		$job->delete();
 	}
 
@@ -241,18 +247,23 @@ class Mailer {
 	 * @param  array   $data
 	 * @return void
 	 */
-	protected function addContent($message, $view, $plain, $data): void
+	protected function addContent(Message $message, ?string $view, ?string $plain, array $data): void
     {
-		if (isset($view))
+		if ($view !== null)
 		{
-			$message->setBody($this->getView($view, $data), 'text/html');
+			$message->html($this->renderView($view, $data));
 		}
 
-		if (isset($plain))
+		if ($plain !== null)
 		{
-			$message->addPart($this->getView($plain, $data), 'text/plain');
+			$message->text($this->renderView($plain, $data));
 		}
 	}
+
+    protected function renderView(string $view, array $data): string
+    {
+        return $this->views->make($view, $data)->render();
+    }
 
 	/**
 	 * Parse the given view name or array.
@@ -262,16 +273,18 @@ class Mailer {
 	 *
 	 * @throws \InvalidArgumentException
 	 */
-	protected function parseView($view): array
+	protected function parseView(string|array $view): array
     {
-		if (is_string($view)) return array($view, null);
+		if (is_string($view)) {
+            return [$view, null];
+        }
 
 		// If the given view is an array with numeric keys, we will just assume that
 		// both a "pretty" and "plain" view were provided, so we will return this
 		// array as is, since must should contain both views with numeric keys.
 		if (is_array($view) && isset($view[0]))
 		{
-			return $view;
+			return [$view[0], $view[1] ?? null];
 		}
 
 		// If the view is an array, but doesn't contain numeric keys, we will assume
@@ -279,9 +292,10 @@ class Mailer {
 		// named keys instead, allowing the developers to use one or the other.
 		elseif (is_array($view))
 		{
-			return array(
-				array_get($view, 'html'), array_get($view, 'text')
-			);
+			return [
+                $view['html'] ?? null,
+                $view['text'] ?? null
+            ];
 		}
 
 		throw new \InvalidArgumentException("Invalid view.");
@@ -323,8 +337,6 @@ class Mailer {
 	 * @param string|\Closure $callback
 	 * @param \Illuminate\Mail\Message $message
 	 * @return mixed
-	 *
-	 * @throws \InvalidArgumentException
 	 */
 	protected function callMessageBuilder(string|Closure $callback, Message $message): mixed
     {
@@ -332,12 +344,10 @@ class Mailer {
 		{
 			return call_user_func($callback, $message);
 		}
-		elseif (is_string($callback))
+		else
 		{
 			return $this->container[$callback]->mail($message);
 		}
-
-		throw new \InvalidArgumentException("Callback is not valid.");
 	}
 
 	/**
@@ -361,24 +371,12 @@ class Mailer {
 	}
 
 	/**
-	 * Render the given view.
-	 *
-	 * @param  string  $view
-	 * @param  array   $data
-	 * @return \Illuminate\View\View
-	 */
-	protected function getView($view, $data)
-    {
-		return $this->views->make($view, $data)->render();
-	}
-
-	/**
 	 * Tell the mailer to not really send messages.
 	 *
 	 * @param  bool  $value
 	 * @return void
 	 */
-	public function pretend($value = true): void
+	public function pretend(bool $value = true): void
     {
 		$this->pretending = $value;
 	}
