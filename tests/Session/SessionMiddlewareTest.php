fddad07b2925ed2b2997cd934dfe20e0ca1a4d6f
AlexzPurewoko <purwoko908@gmail.com>
2025-08-28 15:18:43 +0700

    adding the test to case when session is not used

--- tests/Session/SessionMiddlewareTest.php
+++ tests/Session/SessionMiddlewareTest.php
@@ -69,6 +69,66 @@ class SessionMiddlewareTest extends BackwardCompatibleTestCase
 		$this->assertSame($response, $middleResponse);
 	}
 
+    public function testSessionIsNotUsedWhenRequestHasValidAuthorizationHeader(): void
+    {
+        $request = Symfony\Component\HttpFoundation\Request::create('http://www.foo.com/api/some-api', 'GET', server: [
+            'HTTP_AUTHORIZATION' => 'Bearer 1234567890',
+        ]);
+        $response = new Symfony\Component\HttpFoundation\Response;
+
+        $middle = new Illuminate\Session\Middleware(
+            $app = m::mock(HttpKernelInterface::class),
+            $manager = m::mock(SessionManager::class)
+        );
+
+        $manager->shouldNotReceive('getSessionConfig');
+
+        $manager->shouldReceive('driver')->andReturn($driver = m::mock(Store::class)->makePartial());
+        $driver->shouldNotReceive('setRequestOnHandler');
+        $driver->shouldNotReceive('start');
+        $app->shouldReceive('handle')->once()->with($request, Symfony\Component\HttpKernel\HttpKernelInterface::MAIN_REQUEST, true)->andReturn($response);
+        $driver->shouldNotReceive('save');
+        $driver->shouldNotReceive('getHandler');
+        $driver->shouldNotReceive('getName')->andReturn('name');
+        $driver->shouldNotReceive('getId')->andReturn(1);
+        $driver->shouldNotReceive('setPreviousUrl');
+
+        $middleResponse = $middle->handle($request);
+
+        self::assertSame($response, $middleResponse);
+        self::assertCount(0, $response->headers->getCookies());
+    }
+
+
+
+    public function testSessionIsNotUsedWhenRequestFromOauthApi(): void
+    {
+        $request = Symfony\Component\HttpFoundation\Request::create('http://www.foo.com/api/v1/oauth/token', 'POST');
+        $response = new Symfony\Component\HttpFoundation\Response;
+
+        $middle = new Illuminate\Session\Middleware(
+            $app = m::mock(HttpKernelInterface::class),
+            $manager = m::mock(SessionManager::class)
+        );
+
+        $manager->shouldNotReceive('getSessionConfig');
+
+        $manager->shouldReceive('driver')->andReturn($driver = m::mock(Store::class)->makePartial());
+        $driver->shouldNotReceive('setRequestOnHandler');
+        $driver->shouldNotReceive('start');
+        $app->shouldReceive('handle')->once()->with($request, Symfony\Component\HttpKernel\HttpKernelInterface::MAIN_REQUEST, true)->andReturn($response);
+        $driver->shouldNotReceive('save');
+        $driver->shouldNotReceive('getHandler');
+        $driver->shouldNotReceive('getName')->andReturn('name');
+        $driver->shouldNotReceive('getId')->andReturn(1);
+        $driver->shouldNotReceive('setPreviousUrl');
+
+        $middleResponse = $middle->handle($request);
+
+        self::assertSame($response, $middleResponse);
+        self::assertCount(0, $response->headers->getCookies());
+    }
+
 
 	public function testCheckingForRequestUsingArraySessions()
 	{
