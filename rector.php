d9a9192b504d776e5a45f17fef787dadddf029e2
agis <agis.septiyanto@gmail.com>
2026-05-12 16:26:10 +0700

    remove non breaking changes rule

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector;
 use Rector\Config\RectorConfig;
+use Rector\ValueObject\PhpVersion;
 use Rector\Php83\Rector\BooleanAnd\JsonValidateRector;
 use Rector\Php83\Rector\Class_\ReadOnlyAnonymousClassRector;
 use Rector\Php83\Rector\ClassConst\AddTypeToConstRector;
@@ -13,6 +14,7 @@ use Rector\Php83\Rector\FuncCall\DynamicClassConstFetchRector;
 use Rector\Php83\Rector\FuncCall\RemoveGetClassGetParentClassNoArgsRector;
 
 return RectorConfig::configure()
+    ->withPhpVersion(PhpVersion::PHP_84)
     ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
