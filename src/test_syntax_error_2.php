76f607687cf12ac78856e5eef04ebd32a448461b
agis <agis.septiyanto@gmail.com>
2026-05-12 16:57:03 +0700

    remove unused file

--- src/test_syntax_error_2.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// ${var} string interpolation - deprecated in PHP 8.2, removed in PHP 9
-// but still triggers deprecation warning in PHP 8.4
-// Fix: use {$var} instead
-$name = 'world';
-echo "Hello ${name}!";
